3bbee60401b8be8ea681f50afbc48f6156c60238
hyunki bae <hyunki@illustudio.co.kr>
2020-06-30 15:53:11 +0900

    Updates

---
+++ precache-manifest.ede59266b12a2401dac2fd00d637bf35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18f5690a42104b1931d6f36242c322e7",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "90cbc18f1ec8aab0053b",
+    "url": "/movie_app_2020/static/css/main.f4c13016.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90cbc18f1ec8aab0053b",
+    "url": "/movie_app_2020/static/js/main.be6d9bd2.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
